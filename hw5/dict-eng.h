5bca3d66594f0e1ea8fedfca5d911a76ae3b6a67
Luke Reshwan <reshwan@usc.edu>
2022-09-15 02:57:15 -0700

    Delete hw5 directory

--- hw5/dict-eng.h
+++
@@ -1,8 +0,0 @@
-#ifndef DICTENG_H
-#define DICTENG_H
-#include <set>
-#include <string>
-//extern const std::set<std::string> dictWords; 
-const std::set<std::string>& readDictWords(std::string filename);
-
-#endif
